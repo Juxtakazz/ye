f60ecb8fecb7e6e6978658365eb51675ff408479
ppsak123 <50125559+Juxtakazz@users.noreply.github.com>
2019-05-06 15:46:50 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace jEf
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int bUrh = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= bUrh; x++ )
+            {
+                Console.Write(x + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
