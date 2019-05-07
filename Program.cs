e28d3b1963b28078dc45bdbdbe61abfefa4e089e
Rexter100 <50125716+Rexter100@users.noreply.github.com>
2019-05-07 10:28:02 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,13 +8,18 @@ namespace jEf
         {
             Console.WriteLine("Enter a number: ");
             int bUrh = Convert.ToInt32(Console.ReadLine());
+            
+            int sum = 0;
 
             for (int x = 1; x <= bUrh; x++ )
             {
                 Console.Write(x + " ");
+                sum += x;
             }
 
+            Console.Writeline("The sum is "+ sum); 
             Console.ReadKey();
+            
         }
     }
 }
